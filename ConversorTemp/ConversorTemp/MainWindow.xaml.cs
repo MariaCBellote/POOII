fd9d45dcc415d3681d988314c893fab7217a8647
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-08-22 08:13:38 -0300

    Add files via upload

---
+++ ConversorTemp/ConversorTemp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConversorTemp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string temperatura="";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void tempC_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            temperatura = tempC.Text;
+        }
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+            double f;
+            if (double.TryParse(temperatura, out f))
+            {
+                f = f * 1.8 + 32;
+                texteTempConvertida.Text= f.ToString("F1") + " Fahrenheit";
+            }
+            else
+            {
+                texteTempConvertida.Text = "Valor inválido";
+            }
+        }
+
+       
+    }
+}
