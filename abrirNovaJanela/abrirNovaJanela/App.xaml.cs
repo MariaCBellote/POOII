e911cef633c47b8b34af0814134ff3756b87de9e
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-08-22 08:54:46 -0300

    Add files via upload

---
+++ abrirNovaJanela/abrirNovaJanela/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace abrirNovaJanela
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
