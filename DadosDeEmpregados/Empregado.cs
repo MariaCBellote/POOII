13e15c1364936bcdd43c660fc6abeee1f8186cf3
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-10-24 08:53:03 -0300

    Add files via upload

---
+++ DadosDeEmpregados/Empregado.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosDeEmpregados
+{
+    internal class Empregado
+    {
+        public int Matricula {  get; set; }
+        public String? Nome { get; set; }
+        public String? CPF { get; set; }
+        public String? Endereco { get; set; }    
+
+        public Empregado() { }
+
+        public Empregado( string nome, string cPF, string endereco)
+        {
+            
+            Nome = nome;
+            CPF = cPF;
+            Endereco = endereco;
+        }
+
+        public override string ToString()
+        {
+            return $"Empregado-> Matrícula: {Matricula} | Nome: {Nome} | CPF: {CPF} | Endereço: {Endereco}.";
+            
+        }
+
+        public void salvar(SqlConnection conex) {
+            Console.WriteLine("==Salvando Empregado==");
+            if (Matricula == 0) {
+                var cmd= conex.CreateCommand();
+                cmd.CommandText = "INSERT INTO Empregado (Nome,CPF,Endereco) VAUES (@nome, @cpf, @endereco)";
+
+                cmd.Parameters.Add(new SqlParameter("nome", Nome));
+                cmd.Parameters.Add(new SqlParameter("cpf", CPF));
+                cmd.Parameters.Add(new SqlParameter("endereco", Endereco));
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void pesquisar() { }
+        public void excluir() { }
+        public void alterar() { }
+    }
+}
---
+++ DadosDeEmpregados/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DadosDeEmpregados
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection? conexao = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            String URL= "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = DBDadosEmpregados; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False";
+
+            try
+            {
+                conexao = new SqlConnection(URL);
+                conexao.Open();
+                status.Content = "Conexão OK";
+            }
+            catch (Exception ex) 
+            {
+                status.Content = "Conexão NÃO OK";
+                Console.WriteLine(ex.Message);
+            
+            
+            }
+        
+        }
+
+        private void pesquisar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void salvar_Click(object sender, RoutedEventArgs e)
+        {
+            Empregado emp = new() {
+                CPF = cpf.Text,
+                Nome = nome.Text,
+                Endereco = endereco.Text
+
+            };
+
+        }
+
+        private void excluir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void limpar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
